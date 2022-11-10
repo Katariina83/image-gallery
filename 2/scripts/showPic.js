debfec9c751c4e35bee2672f56fc8923006aa04d
Katariina83 <rikkonenkatariina@gmail.com>
2022-11-10 11:03:15 +0200

    neljä versiota valmis

---
+++ 2/scripts/showPic.js
@@ -0,0 +1,6 @@
+function showPic (whichpic)  //Funktio showPic saa attribuuttina html-koodissa valitun li-elementin sisälllön.
+{
+    var source = whichpic.getAttribute ("href"); //muuttujan source arvoksi sijoitetaan li-elementin attribuutin href arvo.
+    var placeholder = document.getElementById("placeholder");  //muuttujan placeholder arvoksi sijoitetaan html-tiedoston elementti, jonka nimenä (id) on "placeholder"
+    placeholder.setAttribute("src", source);  //placeholder nimisen elementin attribuutin "src" arvoksi sijoitetaan muuttujan source (href) arvo.
+}
